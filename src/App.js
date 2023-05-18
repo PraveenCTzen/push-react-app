ba5c80da9548fd1b43fcadce424a8e671a6cf022
praveen.dangwani <praveen.dangwani@clevertap.com>
2023-05-18 16:30:10 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [pushNotification,setPushNotification] = useState(Boolean)
+
+  // useEffect(()=>{
+    
+  // },[])
+  console.log('checking',Notification.permission,pushNotification)
+  if(Notification.permission == "granted"){
+    console.log('approved');
+    if(!pushNotification){
+      setPushNotification(true)
+    }
+  }else if (Notification.permission == "denied") {
+    console.log('denied');
+    if(pushNotification){
+      setPushNotification(false)
+    }
+  }
+  
+  const RequestPemission = () =>{
+    Notification.requestPermission().then((prem)=>{
+      console.log('hello world',prem);
+      if(prem === "granted"){
+        console.log('permissin granted');
+        setPushNotification(true)
+        new Notification("Example notification")
+      }else{
+        console.log('not got and blocked');
+        setPushNotification(true)
+      }
+    })
+  }
+
   return (
     <div className="App">
+      {/* <div className='box'>
+        box
+      </div> */}
+      {pushNotification ? null :
+      <div className='parent'>
+      <div className='notification'>
+        <p>This app wants to send you notification</p>
+        <div className='permission_btns'> <button onClick={()=>{setPushNotification(true)}}>Not now</button> <button onClick={RequestPemission}>Allow</button> </div>
+      </div>
+      </div> }
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
